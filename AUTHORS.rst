ad0e3f984a6378fb4a44260dfaeec9b9dba3ad5c
d-kiss <speakupness@gmail.com>
2018-04-21 21:34:59 +0300

    Restructured the project to look like an actual python project.

---
+++ AUTHORS.rst
@@ -0,0 +1,15 @@
+Contributions to the fstring project
+=================================
+
+Creator & Maintainer
+--------------------
+
+- `Dan Elkis <github.com/rinslow>`_
+
+
+Contributors
+------------
+
+In chronological order:
+
+- `Amir Taboul <github.com/Amir-Taboul>`_
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='fstring',
+    packages=['fstring'],
+    version='1.4.42',
+    description='Working with strings has never been prettier.',
+    author='Dan Elkis',
+    author_email='elkissdan@gmail.com',
+    license='MIT',
+    zip_safe=False,
+    url='https://github.com/d-kiss/fstring',
+    keywords=['string', 'fstring', 'formatting', 'inline', 'evaluation'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
